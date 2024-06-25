2614ca9e35ed7e482b44524bc6e6320524167fff
bruh00000 <lookpee0911@gmail.com>
2024-06-25 13:51:41 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ color : "pink" , textAlign: "center"}}>Welcome to DevOps</h1>
+      <hr />
+      <h2 style={{ textAlign: "center" }}>My Name is : Song Yohan</h2>
+    </div>
+  )
+}
+
+export default App
